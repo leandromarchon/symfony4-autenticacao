c45c3182dd6da314aa15a35509c777034e955034
Leandro Marchon <marchonleandro@gmail.com>
2018-12-14 10:34:39 -0200

    Configurando o ambiente e conhecendo o security.yaml

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return new Response("<html><body><h1>Admin Page</h1></body></html>");
+    }
+}
